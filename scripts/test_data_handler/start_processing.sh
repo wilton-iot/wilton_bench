756c9a58132a403c27ba51a525ac338a24531263
Mikhail Myasnikov <myasnikov.mike@gmail.com>
2018-01-16 12:09:15 +0300

    data_handler. added tests catalog selection.

--- scripts/test_data_handler/start_processing.sh
+++ scripts/test_data_handler/start_processing.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 set -e
 
+usage="Usage: $0 path_to_tests_catalog\n"
+
+if [[ "$1" = "" ]]; then
+  echo -e $usage
+  exit 0
+fi
+
 scr_cat=$(dirname "$0")
 
 # specify directory with data
-tests_path="$scr_cat/../../tests_data/"
+# tests_path="$scr_cat/../../tests_data/"
+tests_path=$1
 
 # get all catalogs
 catalogs=$(ls $tests_path)
 
 # for each catalog run handler
 for el in $catalogs; do
-	echo "$tests_path$el"
-	$scr_cat/run_handler.sh "$tests_path$el"
+  if [[ -d "$tests_path/$el" ]]; then
+    echo "$tests_path/$el"
+    $scr_cat/run_handler.sh "$tests_path/$el"
+  fi
 done
