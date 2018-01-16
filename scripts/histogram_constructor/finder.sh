bf5f3d4178f897f2ff7e76e7321892b1020b56e6
Mikhail Myasnikov <myasnikov.mike@gmail.com>
2018-01-16 11:51:26 +0300

    histogram_constructor. added tests catalog selection.

--- scripts/histogram_constructor/finder.sh
+++ scripts/histogram_constructor/finder.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-usage="Usage: $0 specifick_word\n \t use 'latency' or 'requests'"
+usage="Usage: $0 specifick_word path_to_tests_catalog\n \t use 'latency' or 'requests' as specifick_word"
 
-if [[ "$1" = "" ]]; then
+if [[ "$1" = "" ]] || [[ "$2" = "" ]]; then
 	echo -e $usage
 	exit 0
 fi
@@ -18,7 +18,8 @@ rm -f $finded_files
 rm -f $copy_cmds
 
 # relative path to find tests data
-find_path="$sc_cat/../../tests_data/"
+# find_path="$sc_cat/../../tests_data/"
+find_path=$2
 
 find $find_path -name *$1.png.dat >> $finded_files
 
